290cf750e53159347ceaef98807429fc587fbb2e
avi-1227 <avinaashprasadd@gmail.com>
2024-05-27 11:53:38 +0530

    Add some comments in project_1.py file

--- day_1/project_1.py
+++ day_1/project_1.py
@@ -1,6 +1,9 @@
-print("Welcome to the Band Name Generator!")
+# In this project I have applied all concepts which toppics I have Learned in Day-1
+
+print("Welcome to the Band Name Generator!") #using print statement
 print("What\'s name of the city you grew up in?")
-city_name = input().title()
+# Here we use title() function to Capitalize first letter of city Name 
+city_name = input().title() 
 print("What\'s your instrument\'s name?")
 instument_name = input().title()
 
